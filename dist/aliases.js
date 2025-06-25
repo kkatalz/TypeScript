482d9afbb9be59a00d056ca2a4d82bfc7c12a3df
kkatalz <zlata.kiev2004@gmail.com>
2025-06-25 22:38:25 +0300

    Add dist folder to gitignore.

--- dist/aliases.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let employeeAlias = {
-    id: 1,
-    name: "Zlata",
-    retire: (date) => {
-        console.log(date);
-    }
-};
-//# sourceMappingURL=aliases.js.map
--- dist/union.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number")
-        return weight * 2.2;
-    return parseInt(weight) * 2.2;
-}
-kgToLbs(10);
-kgToLbs("10");
-//# sourceMappingURL=union.js.map
