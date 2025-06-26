aeca0e0f86dd2bca63eb5860a7e84471e3b46e5e
kkatalz <zlata.kiev2004@gmail.com>
2025-06-26 14:21:39 +0300

    Implement Index signature.

---
+++ src/OOP/indexSignatures.ts
@@ -0,0 +1,11 @@
+class SeatAssignment {
+    // A1, A2, ..
+    // Mosh, John, ..
+    //Index signature property
+    [seatNumber: string] : string;
+}
+
+let seats = new SeatAssignment();
+seats.A1 = 'Mosh';
+seats['A1'] = 'Mosh';
+seats.A2 = 'John';
