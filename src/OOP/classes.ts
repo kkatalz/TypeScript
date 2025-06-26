b4f97493ee56ed2a4221421195b40befbe09fc00
kkatalz <zlata.kiev2004@gmail.com>
2025-06-26 14:14:46 +0300

    Implement getter and setter.

--- src/OOP/classes.ts
+++ src/OOP/classes.ts
@@ -1,22 +1,31 @@
 class Account {
-    readonly id: number;
-    owner: string;
-    balance : number;
     nickname?: string;
 
-    constructor(id: number, owner : string, balance : number){
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
+    constructor(
+        public readonly id: number,
+        public owner : string,
+        private _balance : number){
     }
 
     deposit(amount: number) : void {
         if (amount <=0)
             throw new Error ('Invalid amount');
-        this.balance += amount;
+        this._balance += amount;
+    }
+
+
+    get balance () : number{
+        return this._balance;
+    }
+
+    set balance(value: number){
+        if (value < 0 )
+            throw new Error('Invalid value');
+        this._balance = value;
     }
 }
 
 let account = new Account(1, 'Mosh', 0)
 account.deposit(100);
-console.log(account);
+console.log(account.balance);
+account.balance = 1;
