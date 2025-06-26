cc0bad9ffb27d7819f21e8ce00586f0f35c40a73
kkatalz <zlata.kiev2004@gmail.com>
2025-06-26 17:28:19 +0300

    Implement class Logger and MyPerson.

---
+++ src/exercises/OOP.ts
@@ -0,0 +1,19 @@
+// Define a class called Logger that takes the name of a file in its constructor and provides a method
+//  for writing messages to that file. Don’t worry about the actual file I/O operations. Just define the class with the right members.
+
+class Logger {
+  constructor(public logFile: string) {}
+
+  log(message: string) {
+    console.log(message);
+  }
+}
+
+// Given the Person class below, create a getter for getting the full name of a person.
+class MyPerson {
+  constructor(public firstName: string, public lastName: string) {}
+
+  get fullPersonName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
