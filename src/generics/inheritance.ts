ac55b5b1a12ce8e2e9aeba791b4c829b6dd28f18
kkatalz <zlata.kiev2004@gmail.com>
2025-06-27 13:45:57 +0300

    Use the keyof operator.

--- src/generics/inheritance.ts
+++ src/generics/inheritance.ts
@@ -1,4 +1,4 @@
-interface Product {
+interface ProductInheritance {
   name: string;
   price: number;
 }
@@ -9,26 +9,39 @@ class Store<T> {
   add(obj: T): void {
     this._objects.push(obj);
   }
+
+  // keyof works the next way: If T is Product, then keyof T => "name" | "price".
+  // Any other properties will be unavailable during COMPLIE time
+  find(property: keyof T, value: unknown): T | undefined {
+    return this._objects.find((obj) => obj[property] === value);
+  }
 }
+
+let storeKeyof = new Store<ProductInheritance>();
+storeKeyof.add({ name: "a", price: 1 });
+console.log(storeKeyof.find("name", "a"));
+
+console.log(storeKeyof.find("price", 1));
+// console.log(storeKeyof.find("nonExisting", 1));  // catch error on compile time by using KEYOF
 
 // Pass on the generic type parameter
 class CompressibleStore<T> extends Store<T> {
   compress() {}
 }
 
-let store = new CompressibleStore<Product>();
+let store = new CompressibleStore<ProductInheritance>();
 store.compress();
 
 // Restrict the generic type parameter
 class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
+  findByName(name: string): T | undefined {
     return this._objects.find((obj) => obj.name === name);
   }
 }
 
 // Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(_category: string): Product[] {
+class ProductStore extends Store<ProductInheritance> {
+  filterByCategory(_category: string): ProductInheritance[] {
     return [];
   }
 }
