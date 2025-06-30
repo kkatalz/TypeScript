c46554291e75bb7f07a5fd2c806f83eef216de47
kkatalz <zlata.kiev2004@gmail.com>
2025-06-30 17:41:17 +0300

    Implement accessor decorator.

---
+++ src/decorators/accessors.ts
@@ -0,0 +1,23 @@
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const result = original?.call(this);
+    return typeof result === "string" ? result.toUpperCase() : result;
+  };
+}
+
+class PersonAccessor {
+  constructor(public firstName: string, public lastName: string) {}
+
+  @Capitalize
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let personAccessor = new PersonAccessor("mosh", "hamedani");
+console.log(personAccessor.fullName);
