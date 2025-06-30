c7d067f481d6d26197e6cde15a72a48fc38d6bc4
kkatalz <zlata.kiev2004@gmail.com>
2025-06-30 18:18:26 +0300

    Implement parameter decorator.

---
+++ src/decorators/parameterDecorators.ts
@@ -0,0 +1,19 @@
+type WatchedParameter = {
+  methodName: string;
+  parameterIndex: number;
+};
+
+const watchedParameters: WatchedParameter[] = [];
+
+function Watch(target: any, methodName: string, parameterIndex: number) {
+  watchedParameters.push({
+    methodName,
+    parameterIndex,
+  });
+}
+
+class Vehicle {
+  move(@Watch speed: number) {}
+}
+
+console.log(watchedParameters);
